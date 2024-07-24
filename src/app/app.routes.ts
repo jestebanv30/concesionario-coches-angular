183d7f65195b99c677a160073c5a387ff1084d76
Juanes <valdezjuanes014@gmail.com>
2024-07-23 20:17:55 -0500

    Redirection to main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: "",
+    redirectTo: "autenticacion/inicio-sesion",
+    pathMatch: "full",
+  },
+  {
+    path: "",
     loadChildren: () => import("./feature/feature.module").then(f => f.FeatureModule)
   }
 ];
