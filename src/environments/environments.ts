b6c9c6061b6d9923aa3d2f81eeb6776b46722063
Juanes <valdezjuanes014@gmail.com>
2024-08-05 00:12:41 -0500

    backend path correction

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://concesionariocochesback-production.up.railway.app/api/"
+  apiUrl: "https://concesionariocochesback-production.up.railway.app/api"
 };
